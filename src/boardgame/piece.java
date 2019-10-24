bf37efd511c96070338d2db8910497302a2c8c44
Gorbashy <james.freire@wapnet.com.br>
2019-10-24 14:01:23 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/piece.java
@@ -0,0 +1,19 @@
+package boardgame;
+
+public class piece {
+
+	protected Position position;
+	private Board board;
+	
+	public piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+	
+}
+
+
